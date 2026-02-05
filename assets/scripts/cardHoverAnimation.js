60a0800fb02e1d15734aab11ac0f7399609bc3f1
Arthurrosa1 <arthurrosa3849@gmail.com>
2026-02-05 18:48:43 -0300

    changes not complet

--- assets/scripts/cardHoverAnimation.js
+++ assets/scripts/cardHoverAnimation.js
@@ -1,7 +1,6 @@
 function handleMouseEnter() {
     this.classList.add('s-card--hovered');
     document.body.id = `${this.id}-hovered`;
-
 }
 
 function handleMouseLeave(){
@@ -9,10 +8,8 @@ function handleMouseLeave(){
     document.body.id = '';
 }
 
-
 function addEventListenersToCards() {
     const cardsElements = document.querySelectorAll('.s-card');
-    console.log(cardsElements);
 
     for(let index = 0; index < cardsElements.length; index++) {
         const card = cardsElements[index];
@@ -21,3 +18,20 @@ function addEventListenersToCards() {
 }
 }
 document.addEventListener('DOMContentLoaded', addEventListenersToCards, false);
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+
+function selectCarouselItem(selectedButtonElement) {
+  const selectedItem = selectedButtonElement.id;
+  const carousel = document.querySelector('.s-cards-carousel');
+  const transform = carousel.style.transform;
+  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+  const rotateYDeg = -120 * (Number(selectedItem) - 1);
+  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+  carousel.style.transform = newTransform;
+
+  const activeButtonElement = document.querySelector('.s-controller_button-active');
+  activeButtonElement.classList.remove('s-controller_button-active');
+  selectedButtonElement.classList.add('s-controller_button-active');
+}
